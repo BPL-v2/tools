80b78c36a0a41b6a24321f13f1a202604f8f41e9
Liberatorist <Liberatorist@gmail.com>
2025-07-18 00:58:48 +0200

    adjust tools for current league

--- handle_guild_invites.py
+++ handle_guild_invites.py
@@ -1,9 +1,10 @@
-import json
 import time
 import requests
 import os
 import dotenv
 dotenv.load_dotenv()
+
+event_id = 72796
 
 
 def get_guild_join_requests():
@@ -22,7 +23,7 @@ def get_guild_join_requests():
         '_': '1747331123076',
     }
     response = requests.get(
-        'https://www.pathofexile.com/api/private-league-member/65013',
+        f'https://www.pathofexile.com/api/private-league-member/{event_id}',
         params=params,
         cookies=cookies,
         headers=headers,
@@ -33,7 +34,7 @@ def get_guild_join_requests():
 def get_sorted_users():
     headers = {'Authorization': f'Bearer {os.getenv("BPL_TOKEN")}'}
     response = requests.get(
-        f'https://v2202503259898322516.goodsrv.de/api/events/current/signups', headers=headers)
+        f'{os.environ.get("BPL_BASE_URL")}/events/current/signups', headers=headers)
     return {player["user"]["account_name"] for player in response.json() if player["team_id"] is not None}
 
 
@@ -43,8 +44,26 @@ def accept_guild_invites(members):
     }
     headers = {
         'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
         'content-type': 'application/json',
+        'origin': 'https://www.pathofexile.com',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
+        'sec-ch-ua-arch': '"x86"',
+        'sec-ch-ua-bitness': '"64"',
+        'sec-ch-ua-full-version-list': '"Chromium";v="136.0.0.0", "Brave";v="136.0.0.0", "Not.A/Brand";v="99.0.0.0"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-model': '""',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform-version': '"15.0.0"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
         'user-agent': 'Liberatorist@gmail.com',
+        'x-requested-with': 'XMLHttpRequest',
     }
     json_data = [
 
@@ -55,7 +74,7 @@ def accept_guild_invites(members):
         for member in members
     ]
     return requests.post(
-        'https://www.pathofexile.com/api/private-league-member/65013',
+        f'https://www.pathofexile.com/api/private-league-member/{event_id}',
         cookies=cookies,
         headers=headers,
         json=json_data,
@@ -70,13 +89,12 @@ def handle_guild_invites():
         member for member in get_guild_join_requests() if member["memberName"] not in sorted_users]
     if unknown_users:
         print(
-            f"Unknown users requesting invites: {json.dumps(unknown_users, indent=0)}")
+            f"Unknown users requesting invites: {', '.join([member['memberName'] for member in unknown_users])}")
     if not members_to_add:
         print("No new members to add.")
         return
     response = accept_guild_invites(members_to_add)
     if response.status_code == 200:
-
         print(f"{len(members_to_add)} Invites accepted successfully.")
     else:
         print(f"Failed to accept invites. Status code: {response.status_code}")
@@ -85,6 +103,7 @@ def handle_guild_invites():
 
 if __name__ == "__main__":
     while True:
-        print("Checking for guild invites...")
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+              "Checking for guild invites...")
         handle_guild_invites()
         time.sleep(300)
